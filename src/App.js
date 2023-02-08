77d6dd946b587b62ceb6ce94b752da7e27737f8d
Rusetskaya12 <rusetskaya.ek@gmail.com>
2023-02-08 21:24:31 +0300

    add weather-app-end

--- src/App.js
+++ src/App.js
@@ -1,29 +1,81 @@
 import React from "react";
-import Info from "./components/info"
-import Form from "./components/form"
-import Weather from "./components/weather"
+import Info from "./components/info";
+import Form from "./components/form";
+import Weather from "./components/weather";
 
 const API_KEY = "ba5e161b1bac2b14f71ee5c17ea15e9e";
 
 class App extends React.Component {
+  state = {
+    temp: undefined,
+    city: undefined,
+    country: undefined,
+    sunrise: undefined,
+    sunset: undefined,
+    error: undefined,
+  };
 
   gettingWeather = async (event) => {
     event.preventDefault();
     const city = event.target.elements.city.value;
-    const api_url = await fetch (`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`);
-    const data = await api_url.json();
-    console.log(data);
-  }
 
+    if (city) {
+      const api_url = await fetch(
+        `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`
+      );
+      const data = await api_url.json();
 
-  render () {
+      let sunset = data.sys.sunset;
+      let date = new Date();
+      date.setTime(sunset);
+      let sunset_date =
+        date.getHours() + ":" + date.getMinutes() + ":" + date.getSeconds();
+
+      this.setState({
+        temp: data.main.temp,
+        city: data.name,
+        country: data.sys.country,
+        sunrise: data.sys.sunrise,
+        sunset: data.sys.sunset,
+        error: undefined,
+      });
+    } else {
+      this.setState({
+        temp: undefined,
+        city: undefined,
+        country: undefined,
+        sunrise: undefined,
+        sunset: undefined,
+        error: "Введите название города",
+      });
+    }
+  };
+
+  render() {
     return (
-      <div>
-      <Info />
-      <Form weatherMethod={this.gettingWeather} />
-      <Weather />
+      <div className="wrapper">
+      <div className="main">
+       <div className="container">
+       <div className="row">
+       <div className="col-sm-5 info">
+        <Info />
+        </div>
+        <div className="col-sm-7 form">
+        <Form weatherMethod={this.gettingWeather} />
+        <Weather
+          temp={this.state.temp}
+          city={this.state.city}
+          country={this.state.country}
+          sunrise={this.state.sunrise}
+          sunset={this.state.sunset}
+          error={this.state.error}
+        />
+        </div>
+       </div>
+       </div>
       </div>
-    )
+      </div>
+    );
   }
 }
 
--- src/components/weather.js
+++ src/components/weather.js
@@ -1,13 +1,19 @@
-import React from 'react'
+import React from "react";
 
-class Weather extends React.Component {
-	render () {
-		return (
-			<div>
-				Компонент с погодой
-			</div>
-		);
-	}
-}
+const Weather = (props) => (
+  <div>
+    {props.city && (
+      <div>
+        <p>
+          Месторасполодение: {props.city}, {props.country}{" "}
+        </p>
+        <p>Температура: {props.temp}</p>
+        <p>Восход солнца: {props.sunrise}</p>
+        <p>Заход солнца: {props.sunset}</p>
+      </div>
+    )}
+    <p>{props.error}</p>
+  </div>
+);
 
 export default Weather;
